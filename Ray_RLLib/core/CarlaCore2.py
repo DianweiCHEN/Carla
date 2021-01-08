d21fbf6af68e763b9f0abac2134384fd0b2efe77
Jacopo Bartiromo <32928804+jackbart94@users.noreply.github.com>
2021-01-08 15:49:20 +0100

    Added core folder

---
+++ Ray_RLLib/core/CarlaCore2.py
@@ -0,0 +1,17 @@
+"""
+This is a core that is used for the files under "Test_Code" folder. It just starts the server.
+"""
+
+from .BaseCarlaCore import BaseCarlaCore, CORE_CONFIG
+
+class CarlaCore(BaseCarlaCore):
+    def __init__(self, environment_config, experiment_config, core_config=None):
+        """
+        Initialize the server, clients, hero and sensors
+        :param environment_config: Environment Configuration
+        :param experiment_config: Experiment Configuration
+        """
+        self.core_config = core_config
+        self.environment_config = environment_config
+        self.experiment_config = experiment_config
+        super().init_server()
