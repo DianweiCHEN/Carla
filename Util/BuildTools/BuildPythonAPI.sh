e9403de8a047640326090d2a0309f85f29f909e0
Joel Moriana <joel.moriana@gmail.com>
2021-06-21 17:09:45 +0200

    added target-wheel-platform and no-install-wheel arguments

--- Util/BuildTools/BuildPythonAPI.sh
+++ Util/BuildTools/BuildPythonAPI.sh
@@ -12,12 +12,13 @@ REMOVE_INTERMEDIATE=false
 BUILD_RSS_VARIANT=false
 BUILD_PYTHONAPI=true
 
-OPTS=`getopt -o h --long help,config:,rebuild,clean,rss,carsim,python-version:,packages:,clean-intermediate,all,xml,target-archive:, -n 'parse-options' -- "$@"`
+OPTS=`getopt -o h --long help,config:,rebuild,clean,rss,carsim,python-version:,no-install-wheel,target-wheel-platform:,packages:,clean-intermediate,all,xml,target-archive:, -n 'parse-options' -- "$@"`
 
 eval set -- "$OPTS"
 
 PY_VERSION_LIST=3
-WHEEL_PLATFORM=manylinux_2_27_x86_64
+TARGET_WHEEL_PLATFORM=
+INSTALL_WHEEL=true
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -27,6 +28,12 @@ while [[ $# -gt 0 ]]; do
       shift ;;
     --python-version )
       PY_VERSION_LIST="$2"
+      shift 2 ;;
+    --no-install-wheel )
+      INSTALL_WHEEL=false
+      shift ;;
+    --target-wheel-platform )
+      TARGET_WHEEL_PLATFORM="$2"
       shift 2 ;;
     --rss )
       BUILD_RSS_VARIANT=true;
@@ -89,12 +96,18 @@ if ${BUILD_PYTHONAPI} ; then
   for PY_VERSION in ${PY_VERSION_LIST[@]} ; do
     log "Building Python API for Python ${PY_VERSION}."
 
-    /usr/bin/env python${PY_VERSION} setup.py bdist_wheel --dist-dir dist/.tmp --plat ${WHEEL_PLATFORM}
+    if [[ -z ${TARGET_WHEEL_PLATFORM} ]] ; then
+      /usr/bin/env python${PY_VERSION} setup.py bdist_wheel --dist-dir dist/.tmp
+      cp dist/.tmp/$(ls dist/.tmp | grep .whl) dist
+    else
+      /usr/bin/env python${PY_VERSION} setup.py bdist_wheel --dist-dir dist/.tmp --plat ${TARGET_WHEEL_PLATFORM}
+      auditwheel repair --plat ${TARGET_WHEEL_PLATFORM} --wheel-dir dist dist/.tmp/$(ls dist/.tmp | grep .whl)
+    fi
 
-    WHEEL_NAME=$(ls dist/.tmp | grep .whl)
-    auditwheel repair --plat ${WHEEL_PLATFORM} --wheel-dir dist dist/.tmp/${WHEEL_NAME}
-
-    /usr/bin/env python${PY_VERSION} -m pip install --user --upgrade --force-reinstall dist/${WHEEL_NAME}
+    # Install wheel
+    if ${INSTALL_WHEEL} ; then
+      /usr/bin/env python${PY_VERSION} -m pip install --user --upgrade --force-reinstall dist/$(ls dist/.tmp | grep .whl)
+    fi
 
     rm -rf dist/.tmp
 
