b23164df7044cba27cf2930351d106ef8625dba0
Daniel Santos-Olivan <dsantosolivan@gmail.com>
2021-09-03 09:48:33 +0200

    SimpleVehicle: Change tabs to spaces
    The files were indented using tabs following Unreal style
    and now it is converted to carla style.

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaSimpleVehicle.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaSimpleVehicle.cpp
@@ -8,62 +8,59 @@ FName ACarlaSimpleVehicle::VehicleMovementComponentName(TEXT("MovementComp"));
 FName ACarlaSimpleVehicle::VehicleMeshComponentName(TEXT("VehicleMesh"));
 
 
-ACarlaSimpleVehicle::ACarlaSimpleVehicle(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
+ACarlaSimpleVehicle::ACarlaSimpleVehicle(const FObjectInitializer& ObjectInitializer) 
+  : Super(ObjectInitializer)
 {
-	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(VehicleMeshComponentName);
-	Mesh->SetCollisionProfileName(UCollisionProfile::Vehicle_ProfileName);
-	Mesh->BodyInstance.bSimulatePhysics = true;
-	Mesh->BodyInstance.bNotifyRigidBodyCollision = true;
-	Mesh->BodyInstance.bUseCCD = true;
-	Mesh->bBlendPhysics = true;
-	Mesh->SetGenerateOverlapEvents(true);
-	Mesh->SetCanEverAffectNavigation(false);
-	RootComponent = Mesh;
+  Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(VehicleMeshComponentName);
+  Mesh->SetCollisionProfileName(UCollisionProfile::Vehicle_ProfileName);
+  Mesh->BodyInstance.bSimulatePhysics = true;
+  Mesh->BodyInstance.bNotifyRigidBodyCollision = true;
+  Mesh->BodyInstance.bUseCCD = true;
+  Mesh->bBlendPhysics = true;
+  Mesh->SetGenerateOverlapEvents(true);
+  Mesh->SetCanEverAffectNavigation(false);
+  RootComponent = Mesh;
 
-	VehicleMovement = CreateDefaultSubobject<UWheeledVehicleMovementComponent, USimpleWheeledVehicleMovementComponent>(VehicleMovementComponentName);
-	VehicleMovement->SetIsReplicated(true); // Enable replication by default
-	VehicleMovement->UpdatedComponent = Mesh;
+  VehicleMovement = CreateDefaultSubobject<UWheeledVehicleMovementComponent, USimpleWheeledVehicleMovementComponent>(VehicleMovementComponentName);
+  VehicleMovement->SetIsReplicated(true); // Enable replication by default
+  VehicleMovement->UpdatedComponent = Mesh;
 }
 
 ACarlaSimpleVehicle::~ACarlaSimpleVehicle()
 {
-	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
-
+  // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
 void ACarlaSimpleVehicle::BeginPlay()
 {
-	Super::BeginPlay();
-	
+  Super::BeginPlay();
 }
 
 // Called every frame
 void ACarlaSimpleVehicle::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+  Super::Tick(DeltaTime);
 }
 
 void ACarlaSimpleVehicle::DisplayDebug(UCanvas* Canvas, const FDebugDisplayInfo& DebugDisplay, float& YL, float& YPos)
 {
-	static FName NAME_Vehicle = FName(TEXT("Vehicle"));
+  static FName NAME_Vehicle = FName(TEXT("Vehicle"));
 
-	Super::DisplayDebug(Canvas, DebugDisplay, YL, YPos);
+  Super::DisplayDebug(Canvas, DebugDisplay, YL, YPos);
 
-	if (DebugDisplay.IsDisplayOn(NAME_Vehicle))
-	{
+  if (DebugDisplay.IsDisplayOn(NAME_Vehicle))
+  {
 #if WITH_PHYSX && PHYSICS_INTERFACE_PHYSX
-		GetVehicleMovementComponent()->DrawDebug(Canvas, YL, YPos);
+    GetVehicleMovementComponent()->DrawDebug(Canvas, YL, YPos);
 #endif // WITH_PHYSX
-	}
+  }
 }
 
 class UWheeledVehicleMovementComponent* ACarlaSimpleVehicle::GetVehicleMovementComponent() const
 {
-	return VehicleMovement;
+  return VehicleMovement;
 }
 
 
--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaSimpleVehicle.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaSimpleVehicle.h
@@ -15,59 +15,58 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 UCLASS()
 class CARLA_API ACarlaSimpleVehicle : public APawn
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
 public:
-	ACarlaSimpleVehicle(const FObjectInitializer& ObjectInitializer);
-	~ACarlaSimpleVehicle();
+  ACarlaSimpleVehicle(const FObjectInitializer& ObjectInitializer);
+  ~ACarlaSimpleVehicle();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
 
-	/**  The main skeletal mesh associated with this Vehicle */
-	UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		class USkeletalMeshComponent* Mesh;
+  /**  The main skeletal mesh associated with this Vehicle */
+  UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    class USkeletalMeshComponent* Mesh;
 
-	/** vehicle simulation component */
-	UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		class UWheeledVehicleMovementComponent* VehicleMovement;
+  /** vehicle simulation component */
+  UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    class UWheeledVehicleMovementComponent* VehicleMovement;
 
 
 public:
 
-	/** Name of the MeshComponent. Use this name if you want to prevent creation of the component (with ObjectInitializer.DoNotCreateDefaultSubobject). */
-	static FName VehicleMeshComponentName;
+  /** Name of the MeshComponent. Use this name if you want to prevent creation of the component (with ObjectInitializer.DoNotCreateDefaultSubobject). */
+  static FName VehicleMeshComponentName;
 
-	/** Name of the VehicleMovement. Use this name if you want to use a different class (with ObjectInitializer.SetDefaultSubobjectClass). */
-	static FName VehicleMovementComponentName;
+  /** Name of the VehicleMovement. Use this name if you want to use a different class (with ObjectInitializer.SetDefaultSubobjectClass). */
+  static FName VehicleMovementComponentName;
 
-	/** Util to get the wheeled vehicle movement component */
-	class UWheeledVehicleMovementComponent* GetVehicleMovementComponent() const;
+  /** Util to get the wheeled vehicle movement component */
+  class UWheeledVehicleMovementComponent* GetVehicleMovementComponent() const;
 
-	//~ Begin AActor Interface
-	virtual void DisplayDebug(class UCanvas* Canvas, const FDebugDisplayInfo& DebugDisplay, float& YL, float& YPos) override;
-	//~ End Actor Interface
+  //~ Begin AActor Interface
+  virtual void DisplayDebug(class UCanvas* Canvas, const FDebugDisplayInfo& DebugDisplay, float& YL, float& YPos) override;
+  //~ End Actor Interface
 
-	/** Returns Mesh subobject **/
-	class USkeletalMeshComponent* GetMesh() const { return Mesh; }
-	/** Returns VehicleMovement subobject **/
-	class UWheeledVehicleMovementComponent* GetVehicleMovement() const { return VehicleMovement; }
+  /** Returns Mesh subobject **/
+  class USkeletalMeshComponent* GetMesh() const { return Mesh; }
+  /** Returns VehicleMovement subobject **/
+  class UWheeledVehicleMovementComponent* GetVehicleMovement() const { return VehicleMovement; }
 
 
 private:
 
-	UPROPERTY(Category = "CARLA Simple Wheeled Vehicle", VisibleAnywhere)
-		bool bPhysicsEnabled = true;
+  UPROPERTY(Category = "CARLA Simple Wheeled Vehicle", VisibleAnywhere)
+    bool bPhysicsEnabled = true;
 
-	// Small workarround to allow optional CarSim plugin usage
-	UPROPERTY(Category = "CARLA Simple Wheeled Vehicle", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		UBaseCarlaMovementComponent* BaseMovementComponent = nullptr;
-
+  // Small workarround to allow optional CarSim plugin usage
+  UPROPERTY(Category = "CARLA Simple Wheeled Vehicle", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    UBaseCarlaMovementComponent* BaseMovementComponent = nullptr;
 };
 
 
